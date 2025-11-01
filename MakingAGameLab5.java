2481aa7ae2bf592103c9ceb0ff1989714af00928
Liam O'Connor <139786984+lxam05@users.noreply.github.com>
2025-11-01 23:36:39 +0000

    Add files via upload

---
+++ MakingAGameLab5.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class MakingAGameLab5 {
+    public static void main(String[] args){
+
+        gameBackgroud g = new gameBackgroud();
+
+        while(g.initialValue <= 21) {
+            if(g.initialValue > 21){
+                System.out.println("You went bust");
+            }
+            else if(g.initialValue == 21) {
+                System.out.println("CONGRATULATIONS YOU HIT 21!! ");
+            }
+            else{
+                g.ContinueGame();
+            }
+        }
+    }
+}
+
+/*
+ * 3 outcomes
+ *  1. score 21 - they win
+ *  2. score > 21 - they lose
+ *  3. draw as its a one player game
+ */
+
+ class gameBackgroud{
+
+    int initialValue;
+    String name;
+    String userChoice;
+
+    Random rand = new Random();
+    gameBackgroud.Cards[] c = gameBackgroud.Cards.values();
+    Scanner s = new Scanner(System.in);
+
+
+    enum Cards{
+        Ace(1),
+        two(2),
+        three(3),
+        four(4),
+        five(5),
+        six(6),
+        seven(7),
+        eight(8),
+        nine(9),
+        ten(10),
+        jack(11),
+        queen(12),
+        king(13);
+
+        int cardValue;
+
+        Cards(int cardValue) {
+            this.cardValue = cardValue;
+        }
+
+        public int getCard(){
+            return this.cardValue;
+        }
+    }
+    
+    gameBackgroud(){
+
+        System.out.println("Enter your name: ");
+        this.name = s.nextLine();
+        String name = this.name;
+
+        gameBackgroud.Cards random1 = c[rand.nextInt(c.length)];
+        gameBackgroud.Cards random2 = c[rand.nextInt(c.length)];
+
+        System.out.println("HERE: " + random1.getCard());
+        System.out.println("HERE: " + random2.getCard());
+
+        this.initialValue = (random1.getCard() + random2.getCard());
+        System.out.println("Player: " + name + "; has " + initialValue);
+    }
+
+    int ContinueGame() {
+
+        if(initialValue == 21){
+            return initialValue;
+        }
+        gameBackgroud.Cards r = c[rand.nextInt(c.length)];
+        System.out.println("Do you want to DRAW another card, or HOLD? ");
+        userChoice = s.nextLine();
+
+        if(userChoice.equals("HOLD")){
+            System.out.println("return");
+            return initialValue;
+        }
+        else if(userChoice.equals("DRAW")){
+            this.initialValue += r.getCard();
+            System.out.println("Your value is: " + initialValue);
+        }
+        return initialValue;
+    }
+ }
